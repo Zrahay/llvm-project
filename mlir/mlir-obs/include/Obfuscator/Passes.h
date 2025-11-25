a5db42bbdc2666fb73fd405398970858d660171e
Zrahay <shubhangsinha44@gmail.com>
2025-11-25 22:09:40 +0530

    Added better support for String and Symbol Obfuscation

--- mlir/mlir-obs/include/Obfuscator/Passes.h
+++ mlir/mlir-obs/include/Obfuscator/Passes.h
@@ -11,8 +11,15 @@ namespace obs {
 // ======================= STRING ENCRYPTION PASS ============================
 struct StringEncryptPass 
     : public PassWrapper<StringEncryptPass, OperationPass<ModuleOp>> {
+
   StringEncryptPass() = default;
   StringEncryptPass(const std::string &key) : key(key) {}
+
+  // CLI Support
+  StringRef getArgument() const override { return "string-encrypt"; }
+  StringRef getDescription() const override {
+    return "Encrypt string attributes using XOR";
+  }
 
   void runOnOperation() override;
 
@@ -25,13 +32,21 @@ std::unique_ptr<Pass> createStringEncryptPass(llvm::StringRef key);
 // ======================== SYMBOL OBFUSCATION PASS ==========================
 struct SymbolObfuscatePass
     : public PassWrapper<SymbolObfuscatePass, OperationPass<ModuleOp>> {
+
   SymbolObfuscatePass() = default;
   SymbolObfuscatePass(const std::string &key) : key(key) {}
+
+  //CLI Support
+  StringRef getArgument() const override { return "symbol-obfuscate"; }
+  StringRef getDescription() const override {
+    return "Obfuscate symbol names randomly";
+  }
 
   void runOnOperation() override;
 
   std::string key = "seed";
 };
+
 
 std::unique_ptr<Pass> createSymbolObfuscatePass(llvm::StringRef key);
 
